9dee7f33ac2531d1c76b2ffcace5f9ffdd3f0367
torikraju <torikraju@gmail.com>
2019-08-12 14:08:27 +0600

    Auth Creating a Dashboard-en

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './HOC/Layout';
 import Home from './containers/home';
 import SignIn from './containers/signIn';
+import Dashboard from './containers/admin/dashboard';
 
 const Routes = () => {
   return (
@@ -11,6 +12,7 @@ const Routes = () => {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signIn" exact component={SignIn} />
+        <Route path="/dashboard" exact component={Dashboard} />
       </Switch>
     </Layout>
   );
---
+++ src/containers/admin/dashboard/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import AdminLayout from '../../../HOC/AdminLayout';
+
+const Dashboard = () => {
+  return (
+    <AdminLayout>
+      <div className="user_dashboard">
+        <div>
+                    This is your dashboard.
+        </div>
+      </div>
+    </AdminLayout>
+  );
+};
+
+export default Dashboard;
